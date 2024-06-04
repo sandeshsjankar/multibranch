32f4d910675f610de201fdc08df7f5568e908e1f
sandeshsjankar <sandeshsjankar@gmail.com>
2024-06-04 16:36:00 +0530

    first commit

---
+++ .history/index_20240604161433.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port =8000;
+app.listen(port,()=> {
+console.log("Server is listening on port:" ${port});
+})
+
+
+app.get('/hello', (req, res)=>{
+    res.send('Hello Aayush Shubham');
+    })
